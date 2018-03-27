961823db74ac8a0069d8267d58c05d253064d5bd
Vorrawut Judasri <vorrawutjud@gmail.com>
2018-03-27 21:39:55 +0700

    show the previous moves made in the game

--- src/components/gameMain.js
+++ src/components/gameMain.js
@@ -45,6 +45,17 @@ import calculateWinner from './tools/calculationWinner';
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
+        const moves = history.map((step, move) => {
+            const desc = move ?
+              'Go to move #' + move :
+              'Go to game start';
+            return (
+              <li>
+                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+              </li>
+            );
+        });
+
         let status;
         if (winner) {
         status = 'Winner: ' + winner;
@@ -54,16 +65,16 @@ import calculateWinner from './tools/calculationWinner';
 
         return (
             <div className="game">
-            <div className="game-board">
-                <Board
-                    squares={current.squares}
-                    onClick={(i) => this.handleClick(i)}
-                />
-            </div>
-            <div className="game-info">
-                <div>{/* status */}</div>
-                <ol>{/* TODO */}</ol>
-            </div>
+                <div className="game-board">
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
+                </div>
             </div>
         );
     }
